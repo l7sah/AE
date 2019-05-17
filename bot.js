f34f7b358b165a56a8fdae902d15100b76bc75a8
l7sah <47457444+l7sah@users.noreply.github.com>
2019-05-17 07:29:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setGame("https://www.twitch.tv/eviil17",{type: 'WATCHING'})
+   client.user.setGame(`By | Eviil17`,'https://www.twitch.tv/eviil17');
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
